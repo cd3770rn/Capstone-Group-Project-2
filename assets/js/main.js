2b41be0e213b9886fbd77b6ff7dd01bfe59b78cb
cd3770rn <35351418+cd3770rn@users.noreply.github.com>
2018-09-30 08:55:51 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,20 +2,20 @@ window.onload = function(){
     // Get a reference to the Firebase database
     var database = firebase.database();
 
-    // function to save search entries to database
-    function saveToFirebase(search) {
-        var searchObject = {
-            search: search
-        };
+//     // function to save search entries to database
+//     function saveToFirebase(search) {
+//         var searchObject = {
+//             search: search
+//         };
 
-        database().ref('search-entries').push().set(searchObject)
-            .then(function(snapshot) {
-                success(); // some success method
-            }, function(error) {
-                console.log('error' + error);
-                error(); // some error method
-            });
-    }
+//         database().ref('search-entries').push().set(searchObject)
+//             .then(function(snapshot) {
+//                 success(); // some success method
+//             }, function(error) {
+//                 console.log('error' + error);
+//                 error(); // some error method
+//             });
+//     }
 
-    saveToFirebase("test");
+//     saveToFirebase("test");
 }
