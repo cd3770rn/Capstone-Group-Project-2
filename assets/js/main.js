8bf4788bc5783e72b5f69f643b896ab323a53bbf
cd3770rn <35351418+cd3770rn@users.noreply.github.com>
2018-09-30 08:59:53 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,18 @@ window.onload = function(){
     // Get a reference to the Firebase database
     var database = firebase.firestore();
 
+    var config = {
+          apiKey: "AIzaSyDHar8VdiUJPi5fnayZmc9pgyFyNhyAlxk",
+          authDomain: "twitter-search-wrapper.firebaseapp.com",
+          databaseURL: "https://twitter-search-wrapper.firebaseio.com",
+          projectId: "twitter-search-wrapper",
+          storageBucket: "twitter-search-wrapper.appspot.com",
+          messagingSenderId: "139961615979"
+        };
+    
+    firebase.initializeApp(config);
+    
+    
 //     // function to save search entries to database
 //     function saveToFirebase(search) {
 //         var searchObject = {
