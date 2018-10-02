577f00cae0130af57498e589fffd463836f65e2b
cd3770rn <35351418+cd3770rn@users.noreply.github.com>
2018-10-02 11:52:10 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,6 +50,7 @@ function get(collection, doc) {
 
 function getGiphy(){
   let input = document.getElementById("search").innerText;
+  console.log(input);
   let xhr = $.get("https://api.giphy.com/v1/gifs/search?q=" + input + "&api_key=blYVByaqQPzRnJ2n8uYs3zfe5kSqcMzO&limit=1");
   xhr.done(function (response) {
       console.log("success got data", response);
