37726403ea6041ce46750bbda006a38b3efed80f
cd3770rn <35351418+cd3770rn@users.noreply.github.com>
2018-10-02 21:12:39 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,17 +46,3 @@ function get(collection, doc) {
         console.log("Error getting document:", error);
     });
 }
-
-
-function getGiphy(){
-  let input = $("#search").val();
-  console.log(input);
-  let xhr = $.get("https://api.giphy.com/v1/gifs/search?q=" + input + "&api_key=blYVByaqQPzRnJ2n8uYs3zfe5kSqcMzO&limit=10");
-  xhr.done(function (response) {
-      console.log("success got data", response);
-      var jiffs = response.data
-      for (i in jiffs){
-          $("main").append("<img src='" + jiffs[i].images.original.url + "' style='height=250px; width:250px'/>")
-      }
-  });
-}
