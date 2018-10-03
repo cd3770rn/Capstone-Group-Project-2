4d6cd4551b2fccf735069f4ec0b8e59d806f6730
cd3770rn <35351418+cd3770rn@users.noreply.github.com>
2018-10-02 21:16:30 -0500

    Update results.js

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,3 +1,24 @@
 window.onload = function() {
-  console.log(window.location.search);
+  let params = parseURL();
+  console.log(params)
+}
+
+
+function parseURL(){
+  let url = window.location.search;
+  return decodeURIComponent(url);
+  //return url;
+}
+
+function getGiphy(){
+  let input = $("#search").val();
+  console.log(input);
+  let xhr = $.get("https://api.giphy.com/v1/gifs/search?q=" + input + "&api_key=blYVByaqQPzRnJ2n8uYs3zfe5kSqcMzO&limit=10");
+  xhr.done(function (response) {
+      console.log("success got data", response);
+      var jiffs = response.data
+      for (i in jiffs){
+          $("main").append("<img src='" + jiffs[i].images.original.url + "' style='height=250px; width:250px'/>")
+      }
+  });
 }
