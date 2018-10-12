cbf3f5c5c5a13604a6069cc3c41a9e2387c35cea
Kenya <email@email.com>
2018-10-11 21:50:22 -0500

    user auth updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,89 +1,89 @@
-let database;
-
-window.onload = function(){
-    const config = {
-          apiKey: "AIzaSyDHar8VdiUJPi5fnayZmc9pgyFyNhyAlxk",
-          authDomain: "twitter-search-wrapper.firebaseapp.com",
-          databaseURL: "https://twitter-search-wrapper.firebaseio.com",
-          projectId: "twitter-search-wrapper",
-          storageBucket: "twitter-search-wrapper.appspot.com",
-          messagingSenderId: "139961615979"
-        };
-    
-    firebase.initializeApp(config);
-
-    function login() {
-        function newLoginHappened(user) {
-            if (user) {
-                // User is signed in
-                app(user);
-            } else {
-                var provider = new firebase.auth.GoogleAuthProvider();
-                firebase.auth().signInWithRedirect(provider);
-            }
-        }
-
-        firebase.auth().onAuthStateChanged(newLoginHappened);
-    }
-
-    function app(user) {
-        // user.displayName
-        // user.email
-        // user.photoURL
-        // user.uid
-        document.getElementById("clientName").innerHTML = user.displayName;
-    }
-
-    window.onload = login;
-    
-    database = firebase.firestore();
-    const settings = {
-        timestampsInSnapshots: true
-    };
-    database.settings(settings);
-    enableSlider();
-};
-
-function save(db, collection, doc, json) {
-    db.collection(collection).doc(doc).set(json)
-    .then(function(){
-        console.log("Document successfully written!");
-    })
-    .catch(function(error) {
-        console.error("Error writing document: ", error);
-    });
-}
-
-function save2(db, collection, collection1, doc, doc1, json) {
-    db.collection(collection).doc(doc).collection(collection1).doc(doc1).set(json)
-    .then(function(){
-        console.log("Document successfully written!");
-    })
-    .catch(function(error) {
-        console.error("Error writing document: ", error);
-    });
-}
-    
-function get(collection, doc) {
-    database.collection(collection).doc(doc).get().then(function(doc) {
-        if (doc.exists) {
-            console.log("Document data:", doc.data());
-        } else {
-            // doc.data() will be undefined in this case
-            console.log("No such document!");
-        }
-    }).catch(function(error) {
-        console.log("Error getting document:", error);
-    });
-}
-
-function enableSlider(){
-    $(".slider").change(function() {
-        if ($(".lbl").css("background-color") == "rgb(204, 204, 204)") {
-            $("#form").attr("action", "results-flickr.html");
-        }
-        else if ($(".lbl").css("background-color") == "rgb(241, 241, 241)") {
-            $("#form").attr("action", "results-giphy.html");
-        }
-    });
-}
+// let database;
+//
+// window.onload = function(){
+//     const config = {
+//           apiKey: "AIzaSyDHar8VdiUJPi5fnayZmc9pgyFyNhyAlxk",
+//           authDomain: "twitter-search-wrapper.firebaseapp.com",
+//           databaseURL: "https://twitter-search-wrapper.firebaseio.com",
+//           projectId: "twitter-search-wrapper",
+//           storageBucket: "twitter-search-wrapper.appspot.com",
+//           messagingSenderId: "139961615979"
+//         };
+//
+//     firebase.initializeApp(config);
+//
+//     function login() {
+//         function newLoginHappened(user) {
+//             if (user) {
+//                 // User is signed in
+//                 app(user);
+//             } else {
+//                 var provider = new firebase.auth.GoogleAuthProvider();
+//                 firebase.auth().signInWithRedirect(provider);
+//             }
+//         }
+//
+//         firebase.auth().onAuthStateChanged(newLoginHappened);
+//     }
+//
+//     function app(user) {
+//         // user.displayName
+//         // user.email
+//         // user.photoURL
+//         // user.uid
+//         document.getElementById("clientName").innerHTML = user.displayName;
+//     }
+//
+//     window.onload = login;
+//
+//     database = firebase.firestore();
+//     const settings = {
+//         timestampsInSnapshots: true
+//     };
+//     database.settings(settings);
+//     enableSlider();
+// };
+//
+// function save(db, collection, doc, json) {
+//     db.collection(collection).doc(doc).set(json)
+//     .then(function(){
+//         console.log("Document successfully written!");
+//     })
+//     .catch(function(error) {
+//         console.error("Error writing document: ", error);
+//     });
+// }
+//
+// function save2(db, collection, collection1, doc, doc1, json) {
+//     db.collection(collection).doc(doc).collection(collection1).doc(doc1).set(json)
+//     .then(function(){
+//         console.log("Document successfully written!");
+//     })
+//     .catch(function(error) {
+//         console.error("Error writing document: ", error);
+//     });
+// }
+//
+// function get(collection, doc) {
+//     database.collection(collection).doc(doc).get().then(function(doc) {
+//         if (doc.exists) {
+//             console.log("Document data:", doc.data());
+//         } else {
+//             // doc.data() will be undefined in this case
+//             console.log("No such document!");
+//         }
+//     }).catch(function(error) {
+//         console.log("Error getting document:", error);
+//     });
+// }
+//
+// function enableSlider(){
+//     $(".slider").change(function() {
+//         if ($(".lbl").css("background-color") == "rgb(204, 204, 204)") {
+//             $("#form").attr("action", "results-flickr.html");
+//         }
+//         else if ($(".lbl").css("background-color") == "rgb(241, 241, 241)") {
+//             $("#form").attr("action", "results-giphy.html");
+//         }
+//     });
+// }
