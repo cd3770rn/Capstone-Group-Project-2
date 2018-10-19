f8241d78aa456e2c04b2b45804e3e824affecc85
cd3770rn <35351418+cd3770rn@users.noreply.github.com>
2018-10-18 23:50:39 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,68 +1,140 @@
 let database;
 
 window.onload = function(){
-
-    // commented out below code because now located in index.html file
-    // const config = {
-    //     apiKey: "AIzaSyDHar8VdiUJPi5fnayZmc9pgyFyNhyAlxk",
-    //     authDomain: "twitter-search-wrapper.firebaseapp.com",
-    //     databaseURL: "https://twitter-search-wrapper.firebaseio.com",
-    //     projectId: "twitter-search-wrapper",
-    //     storageBucket: "twitter-search-wrapper.appspot.com",
-    //     messagingSenderId: "139961615979"
-    // };
-    //
-    // firebase.initializeApp(config);
-
-    database = firebase.firestore();
-    const settings = {
-        timestampsInSnapshots: true
-    };
-    database.settings(settings);
-    enableSlider();
+  initDatebase();
 };
 
-function save(db, collection, doc, json) {
-    db.collection(collection).doc(doc).set(json)
-    .then(function(){
-        console.log("Document successfully written!");
-    })
-    .catch(function(error) {
-        console.error("Error writing document: ", error);
+// ====================
+// ===== AUTH =====
+// ====================
+
+function login() {
+  function newLoginHappened(user) {
+    if (user) {
+      // User is signed in
+      app(user);
+    } 
+
+    else {
+      let provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithRedirect(provider);
+    }
+  }
+
+firebase.auth().onAuthStateChanged(newLoginHappened);
+}
+
+function app(user) {
+    // user.email
+    // user.photoURL
+    // user.uid
+    document.getElementById("clientName").innerHTML = user.displayName;
+}
+
+function logOut() {
+    firebase.auth().signOut().then(function() {
+      // Sign-out successful.
+      console.log("Sign-out successful.");
+    }).catch(function(error) {
+      // An error happened.
+      console.log("An error occurred while signing out.");
     });
 }
 
-function save2(db, collection, collection1, doc, doc1, json) {
-    db.collection(collection).doc(doc).collection(collection1).doc(doc1).set(json)
-    .then(function(){
-        console.log("Document successfully written!");
+// ====================
+// ===== FIREBASE =====
+// ====================
+
+function initDatabase(){
+  const config = {
+    apiKey: "AIzaSyDHar8VdiUJPi5fnayZmc9pgyFyNhyAlxk",
+    authDomain: "twitter-search-wrapper.firebaseapp.com",
+    databaseURL: "https://twitter-search-wrapper.firebaseio.com",
+    projectId: "twitter-search-wrapper",
+    storageBucket: "twitter-search-wrapper.appspot.com",
+    messagingSenderId: "139961615979"
+  };
+  
+  const app = firebase.initializeApp(config);
+  const settings = {
+    timestampsInSnapshots: true  
+  };
+  
+  database = firebase.firestore();
+  database.settings(settings);
+}  
+
+function remove(collection, doc) {
+  database.collection(collection).doc(doc).delete().then(function() {
+    console.log("Document successfully deleted!");
+  }).catch(function(error) {
+      alert("An error occurred while processing your request. Please try again.");
+  });  
+}
+
+function removeAll(collection) {
+  let action = confirm("Remove all saved images?");
+  
+  if (action == true) {
+    let docs = getAll(collection);
+    setTimeout(function() {
+      for (let i = 0; i < docs.length; i++){
+        remove(collection, docs[i]);
+      }
+    }, 1000);
+  }
+  else {
+    return;  
+  }
+}
+
+function save(collection, doc, json) {
+  database.collection(collection).doc(doc).set(json)
+  .then(function(){
+      console.log("Document successfully written!");
     })
     .catch(function(error) {
-        console.error("Error writing document: ", error);
+      console.error("Error writing document: ", error);
+      alert("An error occurred while processing your request. Please try again.");
     });
 }
 
 function get(collection, doc) {
     database.collection(collection).doc(doc).get().then(function(doc) {
-        if (doc.exists) {
-            console.log("Document data:", doc.data());
-        } else {
-            // doc.data() will be undefined in this case
-            console.log("No such document!");
-        }
+      if (doc.exists) {
+        console.log("Document data:", doc.data());
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+      }
     }).catch(function(error) {
         console.log("Error getting document:", error);
     });
 }
 
-function enableSlider(){
-    $(".slider").change(function() {
-        if ($(".lbl").css("background-color") == "rgb(204, 204, 204)") {
-            $("#form").attr("action", "results-flickr.html");
-        }
-        else if ($(".lbl").css("background-color") == "rgb(241, 241, 241)") {
-            $("#form").attr("action", "results-giphy.html");
-        }
-    });
+function getWhere(collection, field, operator, expected) {
+  let array = [];
+  database.collection(collection).where(field, operator, expected).get()
+    .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+          // doc.data() is never undefined for query doc snapshots
+          //console.log(doc.id, " => ", doc.data());
+          array.push(doc.data())
+        });
+    })
+  return array;
 }
 
+function getAll(collection) {
+  let array = [];
+  database.collection(collection).get().then(function(querySnapshot) {
+    querySnapshot.forEach(function(doc) {
+      array.push(doc.data());
+    });
+  });
+  return array;
+}
+
+function merge(collection, doc, json) {
+  database.collection(collection).doc(doc).set(json, { merge: true });
+}
