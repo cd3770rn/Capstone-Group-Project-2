963bbebb965ee8a72110e8cc321124cb17beb5ff
cd3770rn <35351418+cd3770rn@users.noreply.github.com>
2018-10-20 06:21:36 -0500

    Update general.js

--- assets/js/general.js
+++ assets/js/general.js
@@ -39,10 +39,23 @@ function login() {
 
     else {
       let provider = new firebase.auth.GoogleAuthProvider();
-      firebase.auth().signInWithRedirect(provider);
+      firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+        .then(function() {
+          // Existing and future Auth states are now persisted in the current
+          // session only. Closing the window would clear any existing state even
+          // if a user forgets to sign out.
+          // ...
+          // New sign-in will be persisted with session persistence.
+          return firebase.auth().signInWithRedirect(provider);
+        })
+        .catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+        });
+      }
     }
-  }
-firebase.auth().onAuthStateChanged(newLoginHappened);
+  firebase.auth().onAuthStateChanged(newLoginHappened);
 }
 
 function app(user) {
