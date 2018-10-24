06f14d225a01bfa5a25cfdcff16e58c36eefe38c
cd3770rn <35351418+cd3770rn@users.noreply.github.com>
2018-10-24 14:41:54 -0500

    Update worker-giphy.js

--- assets/js/worker-giphy.js
+++ assets/js/worker-giphy.js
@@ -1,41 +1,44 @@
-importScripts('//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js');
+if( 'function' === typeof importScripts) {
 
-self.addEventListener('message', function(event) {
-  console.log(event);
-  getGiphy(event.data);
-});
- 
-function parseURL(){
-  // This will remove all of the unwanted characters from the URL and just give you the query string at the end.
-  let url = window.location.search; // get url
-  url = url.replace("?query=", ""); //remove fluff
-  url = url.split("+").join(" "); // convert all + to " " so: cats+and+dogs --> cats and dogs
-  return url;
-}
+  importScripts('//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js');
 
-function createIMG(url) {
-  // Cleaner way of creating an <img> tag than doing it all in one line
-  let divStart = "<div class='img-container'>"
-  let overlay = "<div class='img-overlay'><button class='add-icon' onclick='saveImage($(this))'><svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' version='1.1' x='0px' y='0px' viewBox='0 0 31.444 31.444' xml:space='preserve'><path d='M1.119,16.841c-0.619,0-1.111-0.508-1.111-1.127c0-0.619,0.492-1.111,1.111-1.111h13.475V1.127 C14.595,0.508,15.103,0,15.722,0c0.619,0,1.111,0.508,1.111,1.127v13.476h13.475c0.619,0,1.127,0.492,1.127,1.111 c0,0.619-0.508,1.127-1.127,1.127H16.833v13.476c0,0.619-0.492,1.127-1.111,1.127c-0.619,0-1.127-0.508-1.127-1.127V16.841H1.119z'/><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg></button></div>"
-  let tagStart = "<img src='";
-  let tagEnd = "'/>";
-  let divEnd = "</div>";
-  let img = divStart + overlay + tagStart + url + tagEnd + divEnd;
-  return img;
-}
-
-// API #1 -- Giphy
-function getGiphy(input){
-  let quantity = "10";
-  let search = "https://api.giphy.com/v1/gifs/search?q=" + input + "&api_key=blYVByaqQPzRnJ2n8uYs3zfe5kSqcMzO&limit=" + quantity;
-  let xhr = $.get(search);
-  
-  xhr.done(function (response) {
-      var jiffs = response.data;
-      for (i in jiffs){
-        let imgURL = jiffs[i].images.original.url;
-        img = createIMG(imgURL);
-        $("#img-stack").append(img);
-      }
+  self.addEventListener('message', function(event) {
+    console.log(event);
+    getGiphy(event.data);
   });
+
+  function parseURL(){
+    // This will remove all of the unwanted characters from the URL and just give you the query string at the end.
+    let url = window.location.search; // get url
+    url = url.replace("?query=", ""); //remove fluff
+    url = url.split("+").join(" "); // convert all + to " " so: cats+and+dogs --> cats and dogs
+    return url;
+  }
+
+  function createIMG(url) {
+    // Cleaner way of creating an <img> tag than doing it all in one line
+    let divStart = "<div class='img-container'>"
+    let overlay = "<div class='img-overlay'><button class='add-icon' onclick='saveImage($(this))'><svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' version='1.1' x='0px' y='0px' viewBox='0 0 31.444 31.444' xml:space='preserve'><path d='M1.119,16.841c-0.619,0-1.111-0.508-1.111-1.127c0-0.619,0.492-1.111,1.111-1.111h13.475V1.127 C14.595,0.508,15.103,0,15.722,0c0.619,0,1.111,0.508,1.111,1.127v13.476h13.475c0.619,0,1.127,0.492,1.127,1.111 c0,0.619-0.508,1.127-1.127,1.127H16.833v13.476c0,0.619-0.492,1.127-1.111,1.127c-0.619,0-1.127-0.508-1.127-1.127V16.841H1.119z'/><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg></button></div>"
+    let tagStart = "<img src='";
+    let tagEnd = "'/>";
+    let divEnd = "</div>";
+    let img = divStart + overlay + tagStart + url + tagEnd + divEnd;
+    return img;
+  }
+
+  // API #1 -- Giphy
+  function getGiphy(input){
+    let quantity = "10";
+    let search = "https://api.giphy.com/v1/gifs/search?q=" + input + "&api_key=blYVByaqQPzRnJ2n8uYs3zfe5kSqcMzO&limit=" + quantity;
+    let xhr = $.get(search);
+
+    xhr.done(function (response) {
+        var jiffs = response.data;
+        for (i in jiffs){
+          let imgURL = jiffs[i].images.original.url;
+          img = createIMG(imgURL);
+          $("#img-stack").append(img);
+        }
+    });
+  }
 }
