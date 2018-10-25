82ca3ae07c5543b33b2d7f1209d7421b2254ee15
cd3770rn <35351418+cd3770rn@users.noreply.github.com>
2018-10-25 10:17:52 -0500

    Update results.js

--- assets/js/results.js
+++ assets/js/results.js
@@ -92,18 +92,19 @@ function populatePage(input) {
 function startWorker(giphyInput, flickrInput) {
   let output = [];
   setTimeout(function() {
+    // New thread
     if (window.Worker) {
-      worker = new Worker('/Capstone-Group-Project-2/assets/js/worker.js');
-      worker.postMessage(giphyInput);
-      worker.postMessage(flickrInput);
+      worker = new Worker('/Capstone-Group-Project-2/assets/js/worker.js'); // start worker
+      worker.postMessage(giphyInput); // send API response to worker
+      worker.postMessage(flickrInput); // send API response to worker
       worker.addEventListener('message', function(event) {
         console.log(event.data);
-        output.push(event.data);
+        output.push(event.data); // store worker response
         setTimeout(function() {
           worker.terminate(); 
-        }, 3000);
+        }, 3000); // thread terminates after 3000ms
       });
     }
-  }, 500);
+  }, 500); // 500ms is long enough for elements in arguments to not appear as undefined
 }
 
