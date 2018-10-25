7a1fd32d34f60a7f8892b4e2858617daf70c7290
cd3770rn <35351418+cd3770rn@users.noreply.github.com>
2018-10-25 16:39:08 -0500

    Update saved.js

--- assets/js/saved.js
+++ assets/js/saved.js
@@ -9,6 +9,23 @@ window.onload = function() {
   }, 5000);
 }
 
+function deleteSaved(element) {
+  console.log(getSrc(element));
+  //remove(firebase.auth().currentUser.uid, getSrc(element));  
+}
+
+function getSrc(element) {
+  let src = $(element).closest(".img-container").find("img")[0].src;
+  if (src.includes("giphy")) {
+    return src.match("([^\/])([a-zA-Z0-9]){10,}")[0];
+  }
+  else if (src.includes("flickr")) {
+    return src.match("([^\/\.])([a-zA-Z0-9_]){12,}")[0];
+  }  
+  else if (src.inlcudes("unsplash")) {
+     return src.match("([^\/\.])([a-zA-Z0-9_]){27,}")[0];
+  }
+}
 
 function getSaved() {
   setTimeout(function() {
@@ -26,7 +43,7 @@ function getSaved() {
 function createSavedIMG(url) {
   // Cleaner way of creating an <img> tag than doing it all in one line
   let divStart = "<div class='img-container'>"
-  let overlay = "<div class='img-overlay'><button class='delete-icon' onclick=''><svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' version='1.1' id='Capa_1' x='0px' y='0px' viewBox='0 0 31.112 31.112' style='enable-background:new 0 0 31.112 31.112;' xml:space='preserve'> <polygon points='31.112,1.414 29.698,0 15.556,14.142 1.414,0 0,1.414 14.142,15.556 0,29.698 1.414,31.112 15.556,16.97 29.698,31.112 31.112,29.698 16.97,15.556'/><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg></button></div>"
+  let overlay = "<div class='img-overlay'><button class='delete-icon' onclick='deleteSaved($(this))'><svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' version='1.1' id='Capa_1' x='0px' y='0px' viewBox='0 0 31.112 31.112' style='enable-background:new 0 0 31.112 31.112;' xml:space='preserve'> <polygon points='31.112,1.414 29.698,0 15.556,14.142 1.414,0 0,1.414 14.142,15.556 0,29.698 1.414,31.112 15.556,16.97 29.698,31.112 31.112,29.698 16.97,15.556'/><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg></button></div>"
   let tagStart = "<img src='";
   let tagEnd = "'/>";
   let divEnd = "</div>";
