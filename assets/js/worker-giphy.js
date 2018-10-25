aa4135f38e23ab3a5bfa224b11a2b1ae81cf6d13
cd3770rn <35351418+cd3770rn@users.noreply.github.com>
2018-10-24 20:44:33 -0500

    Update worker-giphy.js

--- assets/js/worker-giphy.js
+++ assets/js/worker-giphy.js
@@ -1,10 +1,8 @@
-return;
-
 self.addEventListener('message', function(event) {
   console.log(event);
   console.log(event.data);
-  let imgs = responseToIMG(event.data);
-  self.postMessage({ response: imgs })
+//   let imgs = responseToIMG(event.data);
+//   self.postMessage({ response: imgs })
 });
 
 function parseURL(){
