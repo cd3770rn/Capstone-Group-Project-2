4cb3965fe0d0043590f25e4773cff952156e58c6
cd3770rn <35351418+cd3770rn@users.noreply.github.com>
2018-10-25 10:28:22 -0500

    Update results.js

--- assets/js/results.js
+++ assets/js/results.js
@@ -69,6 +69,7 @@ function getUnsplash(input){
 
 function parseResponse(response) {
   console.log("Parsing response...");
+  console.log(response);
   for (let i = 0; i < response.length; i++) {
     for (let j = 0; j < response[i].length; j++) {
       console.log(response[i][j]);
